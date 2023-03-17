f4e42f37c8d97988b98e2342b2252afdcae8b34d
ganesh <ganeshsgohane>
2023-03-17 22:10:39 +0530

    hellow

--- Playbook/terraform.tf
+++ Playbook/terraform.tf
@@ -1,8 +1,8 @@
 provider "aws"  {    
      region = "us-east-2"
 } 
- resource "aws_instance" "web-server"{
-    ami = ami-05a36e1502605b4aa
+ resource "aws_instance" "web-server" {
+    ami = "ami-05a36e1502605b4aa"
     instance_type = "t2.micro"
     security_groups = ["default"]
     key_name= "GaneshO.pem"
