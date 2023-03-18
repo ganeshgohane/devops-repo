963e9b1933599117b04c9444939b4f7f04e94655
ganesh <ganeshsgohane>
2023-03-18 20:43:37 +0530

    dev

--- Playbook/terraform.tf
+++ Playbook/terraform.tf
@@ -5,6 +5,6 @@ provider "aws" {
 resource "aws_instance" "web-server" {
   ami = "ami-05a36e1502605b4aa"
   instance_type = "t2.micro"
-  security_groups_id = ["sg-025d23bdcd04f8477"]
+  security_group_id = ["sg-025d23bdcd04f8477"]
   key_name = "GaneshO"
 }
