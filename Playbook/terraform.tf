195e294c784f28f7e6230a412fa68b51a851b2b0
ganesh <ganeshsgohane>
2023-03-18 20:28:07 +0530

    dev

--- Playbook/terraform.tf
+++ Playbook/terraform.tf
@@ -1,10 +1,10 @@
-provider "aws"  {    
-     region = "us-east-2"
-} 
- resource "aws_instance" "web-server" {
-    ami = "ami-05a36e1502605b4aa"
-    instance_type = "t2.micro"
-    security_groups = ["default"]
-    key_name= "GaneshO.pem"
-    }
+provider "aws" {
+  region = "us-east-2"
+}
 
+resource "aws_instance" "web-server" {
+  ami = "ami-05a36e1502605b4aa"
+  instance_type = "t2.micro"
+  security_groups = ["default"]
+  key_name = "GaneshO"
+}
